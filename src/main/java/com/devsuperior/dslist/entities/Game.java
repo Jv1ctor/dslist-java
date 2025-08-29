c76e01e349f8337e11646565087ddf31cb969ec8
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-08-29 18:19:09 -0300

    feat: add properties, Game entity, ORM

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,17 +1,25 @@
-package com.devsuperior.dslist;
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_game")
 public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platform;
     private String imgUrl;
     private String shortDescription;
     private String longDescription;
-
 
     public Game(){}
 
